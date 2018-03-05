98dbedac074b18b5226db0f26aa06cbd83c989f1
timothy <timothy.debock.apollo@gmail.com>
2018-03-05 16:07:28 +0100

    unused label

--- src/community/taskmanager/src/main/java/org/geoserver/taskmanager/web/AbstractConfigurationsPage.java
+++ src/community/taskmanager/src/main/java/org/geoserver/taskmanager/web/AbstractConfigurationsPage.java
@@ -26,7 +26,6 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
