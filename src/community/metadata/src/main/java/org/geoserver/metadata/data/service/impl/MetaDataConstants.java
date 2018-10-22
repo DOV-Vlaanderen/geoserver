a047708eb8f6f1aa179c1b47ddbd8edaa16ddbfd
Niels Charlier <niels@scitus.be>
2018-10-22 11:48:00 +0200

    add missing files

---
+++ src/community/metadata/src/main/java/org/geoserver/metadata/data/service/impl/MetaDataConstants.java
@@ -0,0 +1,10 @@
+package org.geoserver.metadata.data.service.impl;
+
+public final class MetaDataConstants {
+    
+    public final static String DIRECTORY = "metadata";
+    
+    public final static String FEATURE_CATALOG = "featureCatalog.yaml";
+    
+    private MetaDataConstants() {}
+}
