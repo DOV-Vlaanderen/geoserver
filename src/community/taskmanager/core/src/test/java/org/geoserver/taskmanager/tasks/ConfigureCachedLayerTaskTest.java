0844eb7de15ee254f9d28bfd72350d6078127f8d
Niels Charlier <niels@scitus.be>
2020-04-23 11:19:15 +0200

    taskmanager: fix gwc tests

--- src/community/taskmanager/core/src/test/java/org/geoserver/taskmanager/tasks/ConfigureCachedLayerTaskTest.java
+++ src/community/taskmanager/core/src/test/java/org/geoserver/taskmanager/tasks/ConfigureCachedLayerTaskTest.java
@@ -13,6 +13,7 @@ import java.net.MalformedURLException;
 import java.sql.SQLException;
 import java.util.Collections;
 import org.geoserver.gwc.GWC;
+import org.geoserver.gwc.config.GWCConfig;
 import org.geoserver.gwc.layer.GeoServerTileLayer;
 import org.geoserver.taskmanager.AbstractTaskManagerTest;
 import org.geoserver.taskmanager.data.Batch;
@@ -67,6 +68,10 @@ public class ConfigureCachedLayerTaskTest extends AbstractTaskManagerTest {
 
     @Override
     public boolean setupDataDirectory() throws Exception {
+        GWCConfig config = GWC.get().getConfig();
+        config.setCacheLayersByDefault(false);
+        GWC.get().saveConfig(config);
+
         DATA_DIRECTORY.addWcs11Coverages();
 
         return true;
