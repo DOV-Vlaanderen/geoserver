d82d1fbd3a59ffae01bf90928678588c067b3c34
Niels Charlier <niels@scitus.be>
2022-02-16 10:07:25 +0100

    [GEOS-10390] make SecuredResourceNameChangeListener thread-safe

--- src/main/src/main/java/org/geoserver/security/SecuredResourceNameChangeListener.java
+++ src/main/src/main/java/org/geoserver/security/SecuredResourceNameChangeListener.java
@@ -21,6 +21,7 @@ import org.geoserver.catalog.event.CatalogListener;
 import org.geoserver.catalog.event.CatalogModifyEvent;
 import org.geoserver.catalog.event.CatalogPostModifyEvent;
 import org.geoserver.catalog.event.CatalogRemoveEvent;
+import org.geoserver.platform.resource.Resource.Lock;
 import org.geoserver.security.impl.DataAccessRule;
 import org.geoserver.security.impl.DataAccessRuleDAO;
 import org.geotools.util.logging.Logging;
@@ -51,36 +52,46 @@ public class SecuredResourceNameChangeListener implements CatalogListener {
 
     @Override
     public void handleRemoveEvent(CatalogRemoveEvent event) throws CatalogException {
-        String resourceWorkSpaceName = null;
-        CatalogInfo resourceInfo = event.getSource();
-        String resourceName = null;
-        if (resourceInfo instanceof LayerInfo) {
-            resourceWorkSpaceName =
-                    ((LayerInfo) resourceInfo).getResource().getStore().getWorkspace().getName();
-            resourceName = ((LayerInfo) resourceInfo).getName();
-        } else if (event.getSource() instanceof LayerGroupInfo) {
-            resourceWorkSpaceName = ((LayerGroupInfo) resourceInfo).getWorkspace().getName();
-            resourceName = ((LayerGroupInfo) resourceInfo).getName();
-        }
+        Lock lock = dao.lock();
+        try {
+            String resourceWorkSpaceName = null;
+            CatalogInfo resourceInfo = event.getSource();
+            String resourceName = null;
+            if (resourceInfo instanceof LayerInfo) {
+                resourceWorkSpaceName =
+                        ((LayerInfo) resourceInfo)
+                                .getResource()
+                                .getStore()
+                                .getWorkspace()
+                                .getName();
+                resourceName = ((LayerInfo) resourceInfo).getName();
+            } else if (event.getSource() instanceof LayerGroupInfo) {
+                resourceWorkSpaceName = ((LayerGroupInfo) resourceInfo).getWorkspace().getName();
+                resourceName = ((LayerGroupInfo) resourceInfo).getName();
+            }
 
-        if (resourceWorkSpaceName != null && resourceInfo != null) {
-            // if a security rule exists for the layer whose name has been changed
-            if (layerHasSecurityRule(resourceWorkSpaceName, resourceName)) {
-                LOGGER.info(
-                        String.format(
-                                "Removing Security Rules for removed layer: %s", resourceName));
+            if (resourceWorkSpaceName != null && resourceInfo != null) {
+                // if a security rule exists for the layer whose name has been changed
+                if (layerHasSecurityRule(resourceWorkSpaceName, resourceName)) {
+                    LOGGER.info(
+                            String.format(
+                                    "Removing Security Rules for removed layer: %s", resourceName));
 
-                List<DataAccessRule> rulesToRemove = getDataAccessRule(resourceInfo, resourceName);
-                for (DataAccessRule r : rulesToRemove) {
-                    dao.removeRule(r);
-                }
-                try {
-                    dao.storeRules();
-                } catch (IOException e) {
-                    LOGGER.log(Level.SEVERE, e.getMessage(), e);
-                    throw new CatalogException(e);
+                    List<DataAccessRule> rulesToRemove =
+                            getDataAccessRule(resourceInfo, resourceName);
+                    for (DataAccessRule r : rulesToRemove) {
+                        dao.removeRule(r);
+                    }
+                    try {
+                        dao.storeRules();
+                    } catch (IOException e) {
+                        LOGGER.log(Level.SEVERE, e.getMessage(), e);
+                        throw new CatalogException(e);
+                    }
                 }
             }
+        } finally {
+            lock.release();
         }
     }
 
@@ -91,61 +102,70 @@ public class SecuredResourceNameChangeListener implements CatalogListener {
 
     @Override
     public void handlePostModifyEvent(CatalogPostModifyEvent event) throws CatalogException {
-        // no names where change
-        if (event.getPropertyNames().indexOf("name") == -1) return;
-        String oldName =
-                String.valueOf(event.getOldValues().get(event.getPropertyNames().indexOf("name")));
-        String newName =
-                String.valueOf(event.getNewValues().get(event.getPropertyNames().indexOf("name")));
-        // dont go anything if name has not changed
-        if (oldName.equalsIgnoreCase(newName)) return;
-        // will be used if name of
-        String resourceWorkSpaceName = null;
-        CatalogInfo resourceInfo = null;
-        // if workspace has been renamed
-        if (event.getSource() instanceof WorkspaceInfo) {
-            // if a security rule exists for the workspaec whose name has been changed
-            if (workspaceHasSecurityRule(oldName)) {
-                LOGGER.info(
-                        String.format(
-                                "Updating Security Rules for Renamed Workspace: %s", oldName));
-
-                // get rules in which workspace name should be updated
-                List<DataAccessRule> rulesToModifyList =
-                        getDataAccessRule(event.getSource(), oldName);
-                for (DataAccessRule rule : rulesToModifyList) rule.setRoot(newName);
-            }
-        } else if (event.getSource() instanceof ResourceInfo) {
-            // if a layer has been renamed
-            // similar layer names can exist in different workspaces
-            resourceInfo = event.getSource();
-            resourceWorkSpaceName =
-                    ((ResourceInfo) resourceInfo).getStore().getWorkspace().getName();
-
-        } else if (event.getSource() instanceof LayerGroupInfo) {
-            // if a layergroup has been renamed
-            // similar layergroup names can exist in different workspaces
-            resourceInfo = event.getSource();
-            resourceWorkSpaceName = ((LayerGroupInfo) resourceInfo).getWorkspace().getName();
-        }
-
-        if (resourceWorkSpaceName != null && resourceInfo != null) {
-            // if a security rule exists for the layer whose name has been changed
-            if (layerHasSecurityRule(resourceWorkSpaceName, oldName)) {
-                LOGGER.info(
-                        String.format(
-                                "Updating Security Rules for Renamed Feature Type: %s", oldName));
-
-                // get rules in which workspace name should be updated
-                List<DataAccessRule> rulesToModifyList = getDataAccessRule(resourceInfo, oldName);
-                for (DataAccessRule rule : rulesToModifyList) rule.setLayer(newName);
-            }
-        }
-
+        Lock lock = dao.lock();
         try {
-            dao.storeRules();
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            // no names where change
+            if (event.getPropertyNames().indexOf("name") == -1) return;
+            String oldName =
+                    String.valueOf(
+                            event.getOldValues().get(event.getPropertyNames().indexOf("name")));
+            String newName =
+                    String.valueOf(
+                            event.getNewValues().get(event.getPropertyNames().indexOf("name")));
+            // dont go anything if name has not changed
+            if (oldName.equalsIgnoreCase(newName)) return;
+            // will be used if name of
+            String resourceWorkSpaceName = null;
+            CatalogInfo resourceInfo = null;
+            // if workspace has been renamed
+            if (event.getSource() instanceof WorkspaceInfo) {
+                // if a security rule exists for the workspaec whose name has been changed
+                if (workspaceHasSecurityRule(oldName)) {
+                    LOGGER.info(
+                            String.format(
+                                    "Updating Security Rules for Renamed Workspace: %s", oldName));
+
+                    // get rules in which workspace name should be updated
+                    List<DataAccessRule> rulesToModifyList =
+                            getDataAccessRule(event.getSource(), oldName);
+                    for (DataAccessRule rule : rulesToModifyList) rule.setRoot(newName);
+                }
+            } else if (event.getSource() instanceof ResourceInfo) {
+                // if a layer has been renamed
+                // similar layer names can exist in different workspaces
+                resourceInfo = event.getSource();
+                resourceWorkSpaceName =
+                        ((ResourceInfo) resourceInfo).getStore().getWorkspace().getName();
+
+            } else if (event.getSource() instanceof LayerGroupInfo) {
+                // if a layergroup has been renamed
+                // similar layergroup names can exist in different workspaces
+                resourceInfo = event.getSource();
+                resourceWorkSpaceName = ((LayerGroupInfo) resourceInfo).getWorkspace().getName();
+            }
+
+            if (resourceWorkSpaceName != null && resourceInfo != null) {
+                // if a security rule exists for the layer whose name has been changed
+                if (layerHasSecurityRule(resourceWorkSpaceName, oldName)) {
+                    LOGGER.info(
+                            String.format(
+                                    "Updating Security Rules for Renamed Feature Type: %s",
+                                    oldName));
+
+                    // get rules in which workspace name should be updated
+                    List<DataAccessRule> rulesToModifyList =
+                            getDataAccessRule(resourceInfo, oldName);
+                    for (DataAccessRule rule : rulesToModifyList) rule.setLayer(newName);
+                }
+            }
+
+            try {
+                dao.storeRules();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            }
+        } finally {
+            lock.release();
         }
     }
 
