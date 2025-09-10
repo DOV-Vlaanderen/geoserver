be4cb6f861b842d542ec738e5dc6290f3d13618c
NielsCharlier <niels@scitus.be>
2025-09-10 15:31:48 +0200

    [GEOS-11930] OGC-API extension breaks security REST API

---
+++ src/extension/ogcapi/ogcapi-core/src/test/java/org/geoserver/ogcapi/ByteArrayMessageConverterTest.java
@@ -0,0 +1,26 @@
+package org.geoserver.ogcapi;
+
+import static org.junit.Assert.assertFalse;
+
+import org.apache.commons.lang3.reflect.TypeUtils;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.rest.catalog.DataStoreController;
+import org.geoserver.rest.security.DataAccessController;
+import org.geoserver.rest.security.RuleMap;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+/** Test Class for ByteArrayMessageConverter */
+public class ByteArrayMessageConverterTest {
+
+    private ByteArrayMessageConverter converter = new ByteArrayMessageConverter();
+
+    @Test
+    public void testReadingNotSupported() {
+        assertFalse(converter.canRead(DataStoreInfo.class, DataStoreController.class, MediaType.APPLICATION_XML));
+        assertFalse(converter.canRead(
+                TypeUtils.parameterize(RuleMap.class, String.class, String.class),
+                DataAccessController.class,
+                MediaType.APPLICATION_XML));
+    }
+}
