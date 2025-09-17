7f445522a50f24c2fc696f9b89e208bd79076c09
Niels Charlier <niels@scitus.be>
2025-09-17 09:35:32 +0200

    [GEOS-11936] fix ogcapi-tiled-features

--- src/community/ogcapi/ogcapi-tiled-features/src/test/java/org/geoserver/ogcapi/v1/features/tiled/ApiTest.java
+++ src/community/ogcapi/ogcapi-tiled-features/src/test/java/org/geoserver/ogcapi/v1/features/tiled/ApiTest.java
@@ -19,11 +19,9 @@ import java.util.List;
 import java.util.logging.Level;
 import org.geoserver.ogcapi.OpenAPIMessageConverter;
 import org.hamcrest.CoreMatchers;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-@Ignore
 public class ApiTest extends TiledFeaturesTestSupport {
 
     @Test
@@ -60,8 +58,8 @@ public class ApiTest extends TiledFeaturesTestSupport {
 
         // ... the tiles themselves
         PathItem tiles =
-                paths.get("/collections/{collectionId}/tiles/{tileMatrixSetId}/{tileMatrix}/{tileRow}/{tileCol}");
+                paths.get("/collections/{vectorCollectionId}/tiles/{tileMatrixSetId}/{tileMatrix}/{tileRow}/{tileCol}");
         assertNotNull(tiles);
-        assertThat(tiles.getGet().getOperationId(), equalTo("getTileOfCollectionId"));
+        assertThat(tiles.getGet().getOperationId(), equalTo("tiles.collection.vector.getTile"));
     }
 }
