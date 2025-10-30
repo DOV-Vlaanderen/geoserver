b452a25ed112c2bc4aebb64d08a8e82ceaeca5ec
Niels Charlier <niels@scitus.be>
2025-10-30 11:46:18 +0100

    EDOVINSPIRE-5296 Nodes in cluster niet meer opvraagbaar

--- src/community/hz-cluster/src/main/java/org/geoserver/cluster/hazelcast/web/HazelcastHomePageContentProvider.java
+++ src/community/hz-cluster/src/main/java/org/geoserver/cluster/hazelcast/web/HazelcastHomePageContentProvider.java
@@ -10,18 +10,14 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.geoserver.cluster.hazelcast.HzCluster;
 import org.geoserver.platform.GeoServerExtensions;
 import org.geoserver.security.GeoServerSecurityManager;
+import org.geoserver.web.GeoServerApplication;
 import org.geoserver.web.GeoServerHomePageContentProvider;
 
 public class HazelcastHomePageContentProvider implements GeoServerHomePageContentProvider {
 
-    HzCluster cluster;
-
-    public HazelcastHomePageContentProvider(HzCluster cluster) {
-        this.cluster = cluster;
-    }
-
     @Override
     public Component getPageBodyComponent(String id) {
+        HzCluster cluster = GeoServerApplication.get().getBeanOfType(HzCluster.class);
         GeoServerSecurityManager secMgr = GeoServerExtensions.bean(GeoServerSecurityManager.class);
         if (secMgr.checkAuthenticationForAdminRole() && cluster.isEnabled()) {
             return new NodeLinkPanel(id, cluster);
